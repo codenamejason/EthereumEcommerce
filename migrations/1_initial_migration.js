d6e730bad45a70aa533d343a482823b605b33de8
Taha <dhaileytaha@gmail.com>
2018-12-31 19:51:53 +0800

    Refresh

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require('./Migrations.sol')
-var EcommerceStore = artifacts.require('./EcommerceStore.sol')
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations)
-  deployer.deploy(EcommerceStore)
-}
